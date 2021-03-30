61ae784129d4b83b9da77f826af6697ff9cb83e3
Collin Winner <cmwinner89@gmail.com>
2021-03-30 17:33:42 -0400

    Finished to initialzation

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -4,23 +4,43 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+ 
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    LIMIT
+      0;
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+      self.columns.each do |name|
+        define_method(name) do
+          self.attributes[name]
+        end
+
+        define_method("#{name}=") do |value|
+          self.attributes[name] = value
+      end
+      end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || 'cats'
   end
 
   def self.all
-    # ...
+    @table_name.each {|col| puts"#{col}"}
   end
 
   def self.parse_all(results)
@@ -32,11 +52,18 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, val|
+      attr_name = attr_name.to_sym
+      if SQLObject.columns.include?(attr_name)
+          self.send("#{attr_name}=", val)
+      else
+        raise "unknown attribute #{attr_name}"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
